abef7c1675c0399fa2a9b277d80a4b53d096aa85
Muhammad Zakria <zakibhai237@gmail.com>
2024-06-27 14:01:17 +0500

    favicon added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Contact from './components/Contact';
 
 function App() {
   const [theme, setTheme] = useState('dark');
+  const [moonClicked, setMoonClicked] = useState(false);
 
   useEffect(() => {
     document.documentElement.classList.add(theme);
@@ -24,18 +25,23 @@ function App() {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
+  const handleMoonClick = () => {
+    toggleTheme();
+    setMoonClicked(!moonClicked);
+  };
+
   return (
     <div className={`overflow-x-hidden antialiased selection:bg-cyan-300 selection:text-cya ${theme === 'dark' ? 'bg-neutral-950 text-neutral-300' : 'bg-white text-neutral-900'}`}>
-      <div className="fixed top-0 -z-10 h-full w-full">
-        {theme === 'dark' ? (
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="fixed top-0 left-0 z-[-2] w-screen h-screen">
+        {moonClicked ? (
+          <div className="bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         ) : (
-          <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+          <div className="bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
         )}
       </div>
       <div className='container mx-auto px-8'>
         <Cursor />
-        <Navbar toggleTheme={toggleTheme} theme={theme} />
+        <Navbar handleMoonClick={handleMoonClick} theme={theme} />
         <Hero />
         <About />
         <Technologies />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { FaLinkedinIn, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import sunImage from '../assets/Theme/sun.png';
 import moonImage from '../assets/Theme/moon.png';
 
-const Navbar = ({ toggleTheme, theme }) => {
+const Navbar = ({ handleMoonClick, theme }) => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         {theme === 'dark' ? (
-          <img src={sunImage} alt="Sun" className="h-8 w-8 cursor-pointer" onClick={toggleTheme} />
+          <img src={sunImage} alt="Sun" className="h-8 w-8 cursor-pointer" onClick={handleMoonClick} />
         ) : (
-          <img src={moonImage} alt="Moon" className="h-8 w-8 cursor-pointer" onClick={toggleTheme} />
+          <img src={moonImage} alt="Moon" className="h-8 w-8 cursor-pointer" onClick={handleMoonClick} />
         )}
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
